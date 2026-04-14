9a6c510b9d13add50dbef07e64133c87c7e82413
Zürcher Patrick (zuercpa2) <zuercpa2@students.zhaw.ch>
2026-04-14 16:22:13 +0200

    Added Main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void QuitGame() {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
